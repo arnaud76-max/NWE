4e0b69faa4072ec7bb0897248179607609b3b351
arnaud76-max <72136722+arnaud76-max@users.noreply.github.com>
2020-12-14 10:59:56 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,7 +87,7 @@ jQuery(function ($) {
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
 				html_body.animate({
-					scrollTop: target.offset().top - 40
+					scrollTop: target.offset().top - 60
 				}, 1500, 'easeInOutExpo');
 				return false;
 			}
